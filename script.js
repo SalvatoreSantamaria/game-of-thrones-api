2d791609132eb7ed02ca691fcb11aadf020167b9
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-06-30 09:58:18 -0700

    Added API search functionality

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const endpoint = "https://www.anapioficeandfire.com/api/characters";
+
 
 const characters = [];
 
-fetch(endpoint)
+const taskInput = document.querySelector('#task');
+
+
+document.getElementById('searchButton').addEventListener('click', function() {
+  
+  console.log('search ran',)
+  const searchInput = document.querySelector('#search')
+  const endpoint = `https://www.anapioficeandfire.com/api/characters/?name=${searchInput.value}`
+
+  console.log(endpoint)
+  fetch(endpoint)
   .then(data => data.json())
-  .then(jsondata => console.log(jsondata))
+ // .then(jsondata => console.log(jsondata))
+    .then(jsondata => characters.push(...jsondata))
+    console.log(characters)
+  });
+
+
+
+
+
+
 
